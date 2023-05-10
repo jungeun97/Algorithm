e69b156bd6ba2dd6072a96428653463e20e0e122
Jungeun <109335512+jungeun97@users.noreply.github.com>
2023-05-10 14:49:35 +0900

    [Bronze V] Title: 아스키 코드, Time: 128 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = br.readLine().charAt(0);
+        bw.write(String.valueOf(n));
+        bw.flush();
+        bw.close();
+    }
+}
