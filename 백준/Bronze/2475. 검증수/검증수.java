42e4b1beec912357376399a8c1e05413d4679224
Jungeun <109335512+jungeun97@users.noreply.github.com>
2023-05-10 14:43:08 +0900

    [Bronze V] Title: 검증수, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int dap = 0;
+        
+        for (int i = 0; i < 5; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            dap += a * a;
+        }
+
+        bw.write(String.valueOf(dap % 10));
+        bw.flush();
+        bw.close();
+    }
+}
