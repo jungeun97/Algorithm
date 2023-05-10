8c6b871d836e94c74c3e7f63dec5ebf591a2e578
Jungeun <109335512+jungeun97@users.noreply.github.com>
2023-05-10 14:44:34 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 224 ms, Memory: 18216 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr1 = new int[n][m];
+        int[][] arr2 = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                int a = Integer.parseInt(st.nextToken());
+                arr1[i][j] = a; 
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                int a = Integer.parseInt(st.nextToken());
+                arr2[i][j] = a;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                bw.write(String.valueOf(arr1[i][j] + arr2[i][j]) + " ");
+            }
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
