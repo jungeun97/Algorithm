9fdf4ec3ba821c75a427fa6162e685f7fe31c72d
Jungeun <109335512+jungeun97@users.noreply.github.com>
2023-05-25 09:35:52 +0900

    [Gold V] Title: 평범한 배낭, Time: 388 ms, Memory: 194320 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,17 @@
+n, k = map(int, input().split())
+dp = [[0] * (k+1) for _ in range(n+1)]
+bag = [[0, 0]]
+for _ in range(n):
+    w, v = map(int, input().split())
+    bag.append([w, v])
+
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        w = bag[i][0]
+        v = bag[i][1]
+        if j < w:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(v + dp[i-1][j-w], dp[i-1][j])
+
+print(dp[n][k])
