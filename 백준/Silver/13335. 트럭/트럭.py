969d973da913cf0fc3934d0b21507095c5f8e4f3
Jungeun <109335512+jungeun97@users.noreply.github.com>
2023-06-23 17:11:09 +0900

    [Silver I] Title: 트럭, Time: 132 ms, Memory: 115136 KB -BaekjoonHub

---
+++ 백준/Silver/13335. 트럭/트럭.py
@@ -0,0 +1,16 @@
+n, w, l = map(int, input().split())
+truck = list(map(int, input().split()))
+
+bridge = [0] * w
+weight, time = 0, 0
+
+while bridge:
+    time += 1
+    bridge.pop(0)
+    if truck:
+        if sum(bridge) + truck[0] <= l:
+            bridge.append(truck.pop(0))
+        else:
+            bridge.append(0)
+
+print(time)
