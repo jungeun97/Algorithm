e407a9b1d9972d15f60a3db830ffa72e34f7c872
Jungeun <109335512+jungeun97@users.noreply.github.com>
2023-08-06 19:05:47 +0900

    [Silver III] Title: 이전 순열, Time: 128 ms, Memory: 115424 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.py
@@ -0,0 +1,13 @@
+
+n = int(input())
+num = list(map(int, input().split()))
+
+for i in range(n-1, 0, -1):
+    if num[i-1] > num[i]:
+        for j in range(n -1, 0, -1):
+            if num[i-1] > num[j]:
+                num[i-1], num[j] = num[j], num[i-1]
+                num = num[:i] + sorted(num[i:], reverse=True)
+                print(*num)
+                exit()
+print(-1)
