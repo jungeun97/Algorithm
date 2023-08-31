8bdc0a7f431aedb6425a1f3b62badf1710e90d14
Jungeun <109335512+jungeun97@users.noreply.github.com>
2023-08-31 16:02:55 +0900

    [Silver I] Title: 정수 삼각형, Time: 160 ms, Memory: 116016 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,16 @@
+n = int(input())
+graph = []
+for _ in range(n):
+    lst = list(map(int, input().split()))
+    graph.append(lst)
+
+for i in range(1, n):
+    for j in range(len(graph[i])):
+        if j == 0:
+            graph[i][j] = graph[i-1][j] + graph[i][j]
+        elif j == len(graph[i]) - 1:
+            graph[i][j] = graph[i][j] + graph[i-1][j-1]
+        else:
+            graph[i][j] = graph[i][j] + max(graph[i-1][j], graph[i-1][j-1])
+
+print(max(graph[n-1]))
